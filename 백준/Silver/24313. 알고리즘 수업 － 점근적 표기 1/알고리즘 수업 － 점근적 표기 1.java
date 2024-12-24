2c6f524491b96211df4ee216e2e83f44730440cd
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2024-12-24 14:03:02 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 108 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -1,28 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        int a,b;
-        a= Integer.parseInt(st.nextToken());
-        b= Integer.parseInt(st.nextToken());
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        st=new StringTokenizer(br.readLine());
-        int c= Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
 
-        st=new StringTokenizer(br.readLine());
-        int n= Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(bufferedReader.readLine());
+        int n0 = Integer.parseInt(bufferedReader.readLine());
 
-        if(a*n+b<=c*n && a<=c){
-            System.out.println(1);
-        }
-        else System.out.println(0);
-
-
+        int f = a1 * n0 + a0;
+        int g = c * n0;
+        if(f<=g &&  a1<=c) bufferedWriter.write(1+"");
+        else bufferedWriter.write(0+"");
+        bufferedWriter.flush();
+        bufferedWriter.close();
     }
 }
