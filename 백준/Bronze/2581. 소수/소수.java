a68bae2252aaea78151cb17fa54882f7059ff9c9
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2024-12-24 12:19:30 +0900

    [Bronze II] Title: 소수, Time: 116 ms, Memory: 15920 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,50 +1,49 @@
-import java.io.IOException;
-import java.util.Arrays;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
+import java.io.*;
 
 public class Main {
 
+    static boolean[] prime;
 
     public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(bufferedReader.readLine());
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int sum=0;
+        int min = Integer.MAX_VALUE;
+        prime = new boolean[n+1];
+        getPrime();
 
-        int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt((br.readLine()));
-        int collect[] = new int[b - a+1];
-        int k = 0;
-
-        for (int i = a; i <= b; i++) {
-            if(b==1){
-                break;
-            }
-            if (i == 1) {
-                i++;
-            }
-            if (i == 2) {
-                collect[0] = 2;
-                k++;
-
-            }
-
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    break;
-                }
-                else if (j == i - 1) {
-
-                    collect[k] += i;
-                    k++;
-                }
+        for(int i=m; i<=n; i++){
+            if(!prime[i]){
+                sum+=i;
+                min = Math.min(min, i);
             }
         }
-        if (k == 0) {
-            System.out.println(-1);
-        } else {
-            System.out.println(Arrays.stream(collect).sum());
-            System.out.println(collect[0]);
+
+        if(sum == 0 )
+            bufferedWriter.write("-1");
+        else{
+            bufferedWriter.write(sum+"\n");
+            bufferedWriter.write(min+"");
         }
+            
+        bufferedWriter.flush();
     }
+
+    static void getPrime(){
+        prime[0] = true;
+        prime[1] = true;
+
+        for(int i =2; i<Math.sqrt(prime.length); i++){
+            if(prime[i]) continue;
+            for(int j=i*i; j< prime.length; j+=i){
+                prime[j] = true;
+            }
+        }
+
+    }
+
+
 }
