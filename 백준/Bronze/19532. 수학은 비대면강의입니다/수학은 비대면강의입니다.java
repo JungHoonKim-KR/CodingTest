5da64a6eb5951877d33512284c9815943091e98e
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2024-12-24 21:49:07 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 196 ms, Memory: 19824 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,12 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
@@ -14,14 +16,19 @@ public class Main {
         int e = Integer.parseInt(st.nextToken());
         int f = Integer.parseInt(st.nextToken());
 
-        for(int x=-999;x<1000;x++){
-            for(int y=-999;y<1000;y++){
-                if(a*x+b*y==c && d*x+e*y==f){
-                    System.out.println(x+" "+y);
-                    break;
+        for(int x = -999; x <= 999; x++) {
+            for(int y = -999; y <= 999; y++) {
+                if(a*x + b*y == c && d*x + e*y == f){
+                    bufferedWriter.write(x + " " + y );
+                    bufferedWriter.flush();
                 }
             }
         }
 
     }
+
+
 }
+
+
+
