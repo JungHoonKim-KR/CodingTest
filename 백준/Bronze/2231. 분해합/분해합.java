03a16a330d372c3d16bc05c282e75406341013ee
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2024-12-26 15:40:19 +0900

    [Bronze II] Title: 분해합, Time: 132 ms, Memory: 14868 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -5,27 +5,26 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        int n = Integer.parseInt(br.readLine());
         int result=0;
-        for(int i =0; i<n;i++){
-            int sum=0;
-            int number = i;
+        for(int i = 0; i <n; i++) {
+            int value = i;
+            int sum= value;
 
-            while(number != 0){
-                sum+= number%10;
-                number/=10;
+            while(value!= 0){
+                sum += value%10;
+                value/=10;
             }
-            if(sum + i == n){
-                result=i;
+            if(sum == n){
+                result = i;
                 break;
             }
         }
-
-        bufferedWriter.write(result+"");
-        bufferedWriter.flush();
-        bufferedWriter.close();
+        bw.write(result+"");
+        bw.flush();
+        bw.close();
     }
 }
