6a73e6bfdf86c6550180245ee674776d7309f991
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2024-12-26 13:12:11 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 120 ms, Memory: 15924 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 97; i<=122; i++){
+            sb.append(s.indexOf((char)i)+" ");
+
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
