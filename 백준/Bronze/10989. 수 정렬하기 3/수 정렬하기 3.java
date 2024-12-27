796f3c2ea93c1fe2083d785856fb899bc892ddba
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2024-12-27 10:33:14 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1660 ms, Memory: 301480 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,34 +1,24 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int n = Integer.parseInt(br.readLine());
         int arr[] = new int[10001];
-
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
-
-        for(int i = 0; i < n; i++) {
-            arr[Integer.parseInt(bufferedReader.readLine())]++;
+        for (int i = 0; i < n; i++) {
+            arr[Integer.parseInt(br.readLine())] ++;
         }
 
-        bufferedReader.close();
-
-        int i=0;
-        while(i<=10000){
-            while(arr[i]>0){
-                bufferedWriter.write(String.valueOf(i));
-                bufferedWriter.newLine();
-                arr[i]--;
+        for(int i=1; i<=10000; i++){
+            for(int j=0; j<arr[i]; j++){
+                bw.write(i+"\n");
             }
-            i++;
         }
-
-        bufferedWriter.flush();
-        bufferedWriter.close();
+        bw.flush();
     }
 }
+
+
