c9b4ca43a5972a3ac2c8dee058c86bd04225dbf3
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2024-12-30 12:21:50 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 160 ms, Memory: 24476 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,32 +1,35 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-import java.util.stream.IntStream;
+import java.util.*;
+
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int size = Integer.parseInt(st.nextToken());
+
+        StringTokenizer st  =new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        Queue<Integer>dataQueue = new LinkedList<>();
-        IntStream.rangeClosed(1,size).forEach(dataQueue::add);
-        bw.write("<");
-        for(int i=0;i<size;i++){
-            int count = 0;
-            int data;
-            while(count != k){
-                data = dataQueue.poll();
-                if(count == k-1 && i != size-1)
-                    bw.write(data+", ");
-                else if(count == k-1 && i == size-1) bw.write(data+">");
-                else dataQueue.add(data);
-                count++;
-            }
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 1; i <=n; i++) {
+            queue.add(i);
         }
+        bw.write("<");
+        while(queue.size()!=0){
+            for(int i =0; i<k-1; i++) {
+                queue.add(queue.poll());
+            }
+            Integer poll = queue.poll();
+            if(queue.size() == n-1)
+                bw.write(poll+"");
+            else bw.write(", "+poll);
+        }
+        bw.write(">");
         bw.flush();
-        bw.close();
     }
+
+
 }
+
+
