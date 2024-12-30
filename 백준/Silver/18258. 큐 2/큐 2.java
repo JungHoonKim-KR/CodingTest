0c0ef8bd81a674875fc3cc155b6372cfd3b7c406
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2024-12-30 11:27:46 +0900

    [Silver IV] Title: 큐 2, Time: 1072 ms, Memory: 383640 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,57 +1,42 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        Queue<Integer> dataQueue = new LinkedList<>();
-        String line, command;
-        int data;
-        for(int i=0;i<n;i++){
-            line = br.readLine();
-            st = new StringTokenizer(line);
-
-            command = st.nextToken();
-
-            if(command.equals("push")){
-                data = Integer.parseInt(st.nextToken());
-                dataQueue.add(data);
+        Queue<String> stack = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            if (command.equals("push")) {
+                stack.add(st.nextToken());
+            } else if (command.equals("pop")) {
+                if (stack.isEmpty()) {
+                    bw.write("-1\n");
+                } else bw.write(stack.poll() + "\n");
+            }else if(command.equals("size")){
+                bw.write(stack.size() + "\n");
+            }else if(command.equals("empty")){
+                if(stack.isEmpty()){bw.write("1\n");}
+                else bw.write("0\n");
+            }else if(command.equals("front")){
+                if(stack.isEmpty()){bw.write("-1\n");}
+                else bw.write(stack.peek()+"\n");
+            }else if(command.equals("back")){
+                if(stack.isEmpty()){bw.write("-1\n");}
+                else bw.write(((LinkedList<String>)stack).getLast()+"\n");
             }
-            else if(command.equals("pop")){
-                if(dataQueue.isEmpty())
-                    bw.write(-1+"\n");
-                else
-                    bw.write(dataQueue.poll()+"\n");
-            }
-            else if(command.equals("size")){
-                bw.write(dataQueue.size()+"\n");
-            }
-            else if(command.equals("empty")){
-                if(dataQueue.isEmpty())
-                    bw.write(1+"\n");
-                else bw.write(0+"\n");
-            }
-            else if(command.equals("front")){
-                if(dataQueue.isEmpty())
-                    bw.write(-1+"\n");
-                else bw.write(dataQueue.peek()+"\n");
-            }
-            else if(command.equals("back")){
-                if(dataQueue.isEmpty())
-                    bw.write(-1+"\n");
-                else bw.write(((LinkedList<Integer>)dataQueue).getLast()+"\n");
-            }
-
-
         }
-        bw.flush();
-        bw.close();
 
+        bw.flush();
     }
+
+
 }
+
+
