e3a65f5f4511f76a0c3468bb2353f70c32578e17
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-03 09:47:33 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 144 ms, Memory: 14896 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -1,25 +1,35 @@
 import java.io.*;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int num = Integer.parseInt(br.readLine());
-        HashSet<String> set = new HashSet<>();
-        set.add("ChongChong");
-        for(int i=0; i<num; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String man1 = st.nextToken();
-            String man2 = st.nextToken();
-            if(set.contains(man1) || set.contains(man2)){
-                set.add(man1);
-                set.add(man2);
+
+        // chongchong이 시작점
+        int n  = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        HashSet<String> set = new HashSet<>(Set.of("ChongChong"));
+        for(int i =0; i<n; i++){
+            // chongchong과 만나는 모든 사람은 감염
+            st = new StringTokenizer(br.readLine());
+            String s1 = st.nextToken();
+            String s2 = st.nextToken();
+            // 만난 사람을 배열에 저장
+            if(set.contains(s1)){
+                set.add(s2);
+            }
+            else if(set.contains(s2)){
+                set.add(s1);
             }
         }
+        // 결과는 set.size()
         bw.write(set.size()+"");
         bw.flush();
-        bw.close();
     }
+
+
 }
+
+
