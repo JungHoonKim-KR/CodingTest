93c5a8f2f3d9268437552040de58c9e4c4ec892e
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-03 19:51:00 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 916 ms, Memory: 131716 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -1,85 +1,85 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 public class Main {
-    private static int count=0;
-    private static int cutLine;
-    private static int result;
-    private static void merge_sort(int [] arr, int left, int right){
-        if(left < right){
-            int mid = (left + right) /2;
-            merge_sort(arr,left,mid);
-            merge_sort(arr,mid+1,right);
-            merge(arr,left,mid,right);
-
-        }
-
-    }
-    private static void merge(int [] arr, int left, int mid, int right){
-        int leftNum = mid - left +1;
-        int rightNum = right  - mid;
-
-        int [] L = new int[leftNum];
-        int [] R = new int[rightNum];
-
-        for(int i = 0;i<leftNum;i++) L[i] = arr[left+i];
-        for(int i=0;i<rightNum;i++) R[i] = arr[mid+1+i];
-
-        int i=0, j=0;
-        int k = left;
-        while(i<leftNum && j<rightNum){
-
-            if(L[i]<R[j]) {
-                arr[k] = L[i];
-                i++;
-            }
-            else {
-                arr[k] = R[j];
-                j++;
-            }
-            count++;
-
-            if(count == cutLine){
-                result = arr[k];
-            }
-            k++;
-        }
-
-        while(i<leftNum){
-            arr[k] = L[i];
-            i++;
-            count++;
-            if(count == cutLine){
-                result = arr[k];
-            }
-            k++;
-        }
-        while(j<rightNum){
-            arr[k] = R[j];
-            j++;
-            count++;
-            if(count == cutLine){
-                result = arr[k];
-            }
-            k++;
-        }
-
-    }
+    static int count=0;
+    static int result =0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int num = Integer.parseInt(st.nextToken());
-        cutLine = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int [] arr = new int[num];
-        for (int i =0; i< num ; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int arr []=Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        merge_sort(arr, 0, arr.length-1, k);
+        if(count<k) 
+            result = -1;
+            
+        bw.write(result+"");
+        bw.flush();;
+        
+
+    }
+
+    static void merge_sort(int arr[], int left, int right, int k){
+        if(left == right) return;
+        int mid = (left + right) /2;
+        merge_sort(arr, left, mid, k);
+        merge_sort(arr, mid+1, right, k);
+        merge(arr, left, mid, right, k);
+    }
+
+    static void merge(int arr[], int left, int mid, int right, int w){
+        int i = left;
+        int j = mid + 1;
+        int temp[] = new int[right - left+1];
+        int tempIdx = 0;
+        while(i<=mid && j<=right){
+            if(arr[i] <= arr[j]){
+                temp[tempIdx++] = arr[i];
+                count++;
+                if(count ==w)
+                    result = arr[i];
+                i++;
+            }
+            else {
+                temp[tempIdx++] = arr[j];
+                count++;
+                if(count ==w)
+                    result = arr[j];
+                j++;
+            }
         }
-        merge_sort(arr,0,num-1);
-        if(count<cutLine) bw.write(-1+"");
-        else bw.write(result+"");
-        bw.flush();
-        bw.close();
+        if(i>mid){
+            while(j<=right){
+                temp[tempIdx++] = arr[j];
+                count++;
+                if(count ==w)
+                    result = arr[j];
+
+                j++;
+            }
+        }
+        if(j>right){
+            while(i<=mid){
+                temp[tempIdx++] = arr[i];
+                count++;
+                if(count ==w)
+                    result = arr[i];
+                i++;
+            }
+        }
+        for(int k = left; k<=right; k++){
+            arr[k] = temp[k - left];
+        }
     }
 }
+
+
