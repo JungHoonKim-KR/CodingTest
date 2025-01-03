dbc900f91ffe674dfdf2fe037279d2ebf9e90b82
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-03 14:20:26 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 744 ms, Memory: 40996 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -1,31 +1,40 @@
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.DelayQueue;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int num = Integer.parseInt(st.nextToken());
-        int std = Integer.parseInt(st.nextToken());
-        Map<String,Integer> data = new HashMap<>();
-        for(int i = 0 ;i<num ; i++){
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
             String s = br.readLine();
-            if(s.length()>=std)
-                data.put(s,data.getOrDefault(s,0)+1);
+            if (s.length() >= m) {
+                map.put(s, map.getOrDefault(s, 0) + 1);
+            }
         }
-        Comparator<Map.Entry<String,Integer>> frequency = Map.Entry.comparingByValue(Comparator.reverseOrder());
-        Comparator<Map.Entry<String,Integer>> length = Map.Entry.comparingByKey(Comparator.comparingInt(String::length).reversed());
-        Comparator<Map.Entry<String,Integer>> alphabet = Map.Entry.comparingByKey();
-
-        Comparator<Map.Entry<String,Integer>> combined = frequency.thenComparing(length).thenComparing(alphabet);
-        List<Map.Entry<String,Integer>> transformedData = new ArrayList<>(data.entrySet());
-
-        Collections.sort(transformedData,combined);
-        for(Map.Entry<String,Integer> entry: transformedData ){
-            bw.write(entry.getKey()+"\n");
+        List<String> words = new ArrayList<>(map.keySet());
+        Collections.sort(words, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(map.get(o1) != map.get(o2))
+                    return map.get(o2) - map.get(o1);
+                else if(o1.length() != o2.length())
+                    return o2.length() - o1.length();
+                return o1.compareTo(o2);
+            }
+        });
+        for(String s : words) {
+            bw.write(s+"\n");
         }
         bw.flush();
         bw.close();
     }
+
+
 }
