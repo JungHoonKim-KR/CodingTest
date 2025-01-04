e0d58b3ab02cd3174f7e81be2dfc0293936e4213
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-04 17:23:03 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 356 ms, Memory: 59376 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/README.md
+++ 백준/Gold/11729. 하노이 탑 이동 순서/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 59632 KB, 시간: 440 ms
+메모리: 59376 KB, 시간: 356 ms
 
 ### 분류
 
@@ -12,7 +12,7 @@
 
 ### 제출 일자
 
-2022년 7월 31일 14:51:26
+2025년 1월 4일 17:22:52
 
 ### 문제 설명
 
@@ -27,7 +27,7 @@
 
 <p>아래 그림은 원판이 5개인 경우의 예시이다.</p>
 
-<p style="text-align: center;"><img alt="" src="" style="height:200px; width:1050px"></p>
+<p style="text-align: center;"><img alt="" src="https://onlinejudgeimages.s3-ap-northeast-1.amazonaws.com/problem/11729/hanoi.png" style="height:200px; width:1050px"></p>
 
 ### 입력 
 
--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,37 +1,39 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.lang.StringBuilder;
-
-
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class Main {
-       static StringBuilder sb = new StringBuilder();
-        public static void main(String[] args) throws IOException {
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
 
-                int n = Integer.parseInt(br.readLine());
-                System.out.println((int)Math.pow(2,n)-1);
-                pole(n,1,3,2);
-                bw.write(sb.toString());
-                bw.flush();
-                bw.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int n = Integer.parseInt(br.readLine());
 
-        }
+        divide(n, 1, 3 ,2);
+        bw.write((int)Math.pow(2,n)-1+"\n");
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();  // 입력 스트림 닫기
+        bw.close();  // 출력 스트림 닫기
 
-        public static void pole(int n,int x,int y,int z){
-                if(n>=1) {
-                        pole(n - 1, x, z, y);
-                        sb.append(x+" "+y+"\n");
-                        pole(n-1,z,y,x);
+    }
 
-                }
+    static void divide(int n, int from, int to, int other) {
+        // n을 옮기기 위해 거쳐가는 구간
+        if(n == 0) return ;
+        divide(n-1, from, other, to);
 
-        }
+        sb.append(from + " "+ to+"\n");
+
+        // n을 목적지로 옮김
+        divide(n-1, other, to, from);
+
+    }
+
 
 }
+
+
