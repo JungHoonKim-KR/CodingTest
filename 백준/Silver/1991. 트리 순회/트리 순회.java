a391bc80e229a2167d2373d128280da3c2da58be
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-04 20:26:26 +0900

    [Silver I] Title: 트리 순회, Time: 108 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -4,8 +4,8 @@ import java.util.stream.Collectors;
 
 
 class Node {
-    char value = 0;
-    Node left = null, right = null;
+    char value;
+    Node left, right;
 
     public Node(char value, Node left, Node right) {
         this.value = value;
@@ -14,48 +14,50 @@ class Node {
 
     }
 }
+ class Tree {
+    Node root = new Node('A', null, null);
+    StringBuilder sb = new StringBuilder();
+    void Insert(Node tmp, char target, char left, char right) {
+        if (tmp == null) return;
 
+        if (root.value == target) {
+            root.left = (left == '.' ? null : new Node(left, null, null));
+            root.right = (right == '.' ? null : new Node(right, null, null));
+        } else if (target == tmp.value) {
+            tmp.left = (left == '.' ? null : new Node(left, null, null));
+            tmp.right = (right == '.' ? null : new Node(right, null, null));
+        } else {
+            Insert(tmp.left, target, left, right);
+            Insert(tmp.right, target, left, right);
+        }
+    }
+    void PreOrder(Node root){
+        if (root == null) return;
+        sb.append(root.value);
+        PreOrder(root.left);
+        PreOrder(root.right);
+    }
+
+    void PostOrder(Node root){
+        if (root == null) return;
+        PostOrder(root.left);
+        PostOrder(root.right);
+        sb.append(root.value);
+
+    }
+
+    void InOrder(Node root){
+        if (root == null) return;
+        InOrder(root.left);
+        sb.append(root.value);
+        InOrder(root.right);
+    }
+}
 
 
 public class Main {
-    static Node root = new Node('A', null, null);
 
-    static class Tree {
-        void Insert(Node tmp, char target, char left, char right) {
-            if (tmp == null) return;
 
-            if (root.value == target) {
-                root.left = (left == '.' ? null : new Node(left, null, null));
-                root.right = (right == '.' ? null : new Node(right, null, null));
-            } else if (target == tmp.value) {
-                tmp.left = (left == '.' ? null : new Node(left, null, null));
-                tmp.right = (right == '.' ? null : new Node(right, null, null));
-            } else {
-                Insert(tmp.left, target, left, right);
-                Insert(tmp.right, target, left, right);
-            }
-        }
-        void PreOrder(Node root){
-            if (root == null) return;
-            System.out.print(root.value);
-            PreOrder(root.left);
-            PreOrder(root.right);
-        }
-
-        void PostOrder(Node root){
-            if (root == null) return;
-            PostOrder(root.left);
-            PostOrder(root.right);
-            System.out.print(root.value);
-        }
-
-        void InOrder(Node root){
-            if (root == null) return;
-            InOrder(root.left);
-            System.out.print(root.value);
-            InOrder(root.right);
-        }
-    }
     StringBuilder sb;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -70,14 +72,17 @@ public class Main {
             char target = st.nextToken().charAt(0);
             char left = st.nextToken().charAt(0);
             char right = st.nextToken().charAt(0);
-            tree.Insert(root,target, left, right);
+            tree.Insert(tree.root,target, left, right);
         }
 
-        tree.PreOrder(root);
-        System.out.println();
-        tree.InOrder(root);
-        System.out.println();
-        tree.PostOrder(root);
+        tree.PreOrder(tree.root);
+        bw.write(tree.sb.toString()+"\n");
+        tree.sb.setLength(0);
+        tree.InOrder(tree.root);
+        bw.write(tree.sb.toString()+"\n");
+        tree.sb.setLength(0);
+        tree.PostOrder(tree.root);
+        bw.write(tree.sb.toString()+"\n");
 
         bw.flush();
         br.close();  // 입력 스트림 닫기
