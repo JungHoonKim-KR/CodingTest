04b1fac34100143f1b8edcd46b80c7bad08cf9ff
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-05 16:56:58 +0900

    [Gold IV] Title: 스도쿠, Time: 444 ms, Memory: 26912 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,116 +1,88 @@
 import java.io.*;
-import java.lang.String;
 import java.util.*;
 
-
-
 public class Main {
-    static int arr[][];
-    static StringBuilder sb = new StringBuilder();
 
+    static int arr[][] = new int[9][9];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        arr=new int[9][9];
-        //문제 받기
-        for (int i = 0; i < 9; i++) {
-           StringTokenizer st=new StringTokenizer(br.readLine()," ");
-            for (int j = 0; j < 9; j++) {
+        StringTokenizer st;
 
+        for(int i =0; i<9;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<9; j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        sudoku(0,0);
+
+        dfs(0,0);
+        br.close();
+        bw.close();
     }
 
-    public static void sudoku(int row,int col) {
-        if(col==9) {
-            sudoku(row + 1, 0);
+
+    static void dfs(int x, int y){
+        if(x ==9){
+            // 다음 행으로 이동
+            dfs(0,y+1);
             return;
         }
-        if (row == 9) {
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    sb.append(arr[i][j] + " ");
+        //탐색 끝
+        if(y==9){
+            StringBuilder sb = new StringBuilder();
+            for(int j=0; j<9; j++){
+                for(int i =0; i<9; i++){
+                    sb.append(arr[j][i]+" ");
                 }
                 sb.append("\n");
             }
             System.out.println(sb);
             System.exit(0);
         }
-
-            if (arr[row][col] == 0) {
-                for (int k = 1; k <= 9; k++) {
-                    if (check(row, col, k)) {
-                        arr[row][col]=k;
-                        sudoku(row,col+1);
-
-                    }
-
+        if(arr[y][x]==0) {
+            for (int i = 1; i <= 9; i++) {
+                if (check(x, y, i)) {
+                    arr[y][x] = i;
+                    dfs(x + 1, y);
                 }
-                arr[row][col]=0;
-                return;
             }
-            sudoku(row,col+1);
+            // 다른 경우의 수 고려해서 초기화
+            arr[y][x] = 0;
+            return;
+        }
+        dfs(x + 1, y);
+    }
 
+    static boolean check(int x, int y, int n){
+        // 열 체크
+        for(int i=0; i<9; i++){
+            if(arr[y][i] == n)
+                return false;
+        }
+        // 행 체크
+        for(int j =0; j<9; j++){
+            if(arr[j][x] ==n)
+                return false;
         }
 
+        //3x3 영역 체크
+        // 해당 영역의 시작점
+        int col = x/3 *3;
+        int row = y/3 *3;
 
-
-    public static boolean check(int y, int x, int value) {
-        for (int i = 0; i < 9; i++) {
-
-                if (arr[y][i] == value)
+        for(int j = row; j<row+3;j++){
+            for(int i = col; i<col+3;i++){
+                if(arr[j][i] == n)
                     return false;
-
-        }
-        for (int j = 0; j < 9; j++) {
-
-                if (arr[j][x] == value)
-                    return false;
-
-        }
-        int X = x / 3 * 3;
-        int Y = y / 3 * 3;
-        for (int row = Y; row < Y + 3; row++) {
-            for (int col = X; col < X + 3; col++) {
-
-                    if (arr[row][col] == value)
-                        return false;
-
             }
         }
+
         return true;
 
+
     }
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
