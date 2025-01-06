7faa4b025ac5948eff992e813ced94b33dcf590e
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-06 11:38:01 +0900

    [Silver I] Title: 스타트와 링크, Time: 336 ms, Memory: 16220 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,56 +1,63 @@
 import java.io.*;
-import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
-
-    static int N;
-    static int[][] score;
-    static boolean[] visit;
-
-    static int Min = Integer.MAX_VALUE;
+    static int arr[][];
+    static int n;
+    static boolean visit[];
+    static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N=Integer.parseInt(br.readLine());
-        score=new int [N][N];
-        visit=new boolean[N];
-        StringTokenizer st;
-        for(int i=0;i<N;i++){
-            st=new StringTokenizer(br.readLine());
-            for(int j=0;j<N;j++){
-                score[i][j]=Integer.parseInt(st.nextToken());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1][n + 1];
+        visit = new boolean[n + 1];
+        for (int j = 1; j <= n; j++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= n; i++) {
+                arr[j][i] = Integer.parseInt(st.nextToken());
             }
         }
-        dfs(0,0);
-        System.out.println(Min);
-
+        dfs(0,1);
+        bw.write(min+"");
+        bw.flush();
     }
-    public static void dfs(int index,int start){
-        if(index==N/2){
-            Min=Math.min(Min,Team());
+
+    static void dfs(int depth, int value) {
+        int team1=0, team2=0;
+        if(depth >= n/2){
+            for(int j=1; j<n; j++){
+                for(int i=j+1; i<=n; i++){
+                    if(visit[i] && visit[j]){
+                        team1 += arr[i][j] + arr[j][i];
+                    }
+                    else if(!visit[i] && !visit[j]){
+                        team2 += arr[i][j] + arr[j][i];
+                    }
+                }
+            }
+
+            min = Math.min(min, Math.abs(team1 - team2));
             return;
         }
-        for(int i=start;i<N;i++){
-            if(!visit[i]){
-                visit[i]=true;
-                dfs(index+1,i+1);
-                visit[i]=false;
+
+        for(int i = value; i<=n; i++){
+            if(!visit[i]) {
+                visit[i] = true;
+                dfs(depth + 1, i + 1);
+                visit[i] = false;
             }
         }
 
+
+
     }
-    public static int Team(){
-        int value1=0;
-        int value2=0;
-        for(int i=0;i<N;i++)
-            for(int j=i+1;j<N;j++){
-                if(visit[i]==true && visit[j]==true)
-                value1+=(score[i][j]+score[j][i]);
-                else if(visit[i]==false && visit[j]==false)
-                value2+=(score[j][i]+score[i][j]);
-        }
-        return Math.abs(value1-value2);
-    }
+
+
+
+
 
 }
