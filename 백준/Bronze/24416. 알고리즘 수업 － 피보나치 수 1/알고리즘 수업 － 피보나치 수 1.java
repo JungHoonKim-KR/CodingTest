9ff7d6a9037e59326aa9cc0e1bba9de818b34e80
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-06 12:40:16 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 496 ms, Memory: 16076 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -1,35 +1,43 @@
 import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class Main{
-    static int count1=0;
-    static int count2=0;
-    static int fib(int n ){
-        if(n==1 || n==2){
-            count1++;
-            return 1;
-        }
-        else return fib(n-1) + fib(n-2);
-    }
-    static int fibonacci(int n){
-        int arr[] = new int[n+1];
-        arr[1]=1; arr[2]=1;
-        for(int i = 3; i<= n; i++){
-            count2++;
-            arr[i] = arr[i-1] + arr[i-2];
-        }
-        return arr[n];
-    }
+public class Main {
+
+    static int fibRecursiveCount = 0;
+    static int fibDPCount=0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int num = Integer.parseInt(br.readLine());
 
-        fib(num);
-        fibonacci(num);
+        int n = Integer.parseInt(br.readLine());
 
-        bw.write(count1 + " "+ count2);
+        fibRecursive(n);
+        int arr[] = new int[n];
+        fibDP(arr, n);
+        bw.write(fibRecursiveCount+ " "+ fibDPCount);
         bw.flush();
-        bw.close();
-
     }
+
+    static int fibRecursive(int n){
+        if(n==1 || n==2){
+            fibRecursiveCount++;
+            return 1;
+        }
+
+        return fibRecursive(n-1) + fibRecursive(n-2);
+    }
+
+    static void fibDP(int arr[], int n){
+        arr[0] = 0; arr[1] = 1;
+        for(int i =2; i<n; i++){
+            fibDPCount++;
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+    }
+
+
+
+
+
 }
