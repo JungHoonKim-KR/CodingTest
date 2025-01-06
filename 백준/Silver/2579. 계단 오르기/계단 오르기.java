d6eeb446d1ba9c2ba5f3c4c390c167b5ae23285b
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-06 20:01:38 +0900

    [Silver III] Title: 계단 오르기, Time: 120 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    static int arr[];
+    static int dp[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        bw.write(fun(n)+"");
+
+        bw.flush();
+    }
+
+
+    static int fun(int n) {
+        
+          dp[1] = arr[1];
+          if(n==1)
+              return dp[1];
+          
+          dp[2] = arr[1] + arr[2];
+
+        if (n >= 3) {
+            dp[3] = arr[3] + Math.max(arr[1],arr[2]);
+            for (int i = 4; i <= n; i++) {
+                dp[i] = arr[i] + Math.max(dp[i - 2], dp[i - 3] + arr[i - 1]);
+            }
+        }
+        return dp[n];
+    }
+}
