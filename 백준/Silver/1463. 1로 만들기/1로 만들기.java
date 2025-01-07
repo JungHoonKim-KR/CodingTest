379036c35b43223a7365760f90169d006ba5e060
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-07 10:53:30 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 16808 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,37 +1,41 @@
 import java.io.*;
-import java.io.IOException;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int num = Integer.parseInt(br.readLine());
-        int dp[]=new int [num+1];
-        dp[0]=0;
-        dp[1]=0;
-        for(int i=2;i<=num;i++){
-            dp[i]=dp[i-1]+1;
-            if(i%3==0){
-                dp[i]=Math.min(dp[i/3]+1,dp[i]);
+
+        int n = Integer.parseInt(br.readLine());
+
+        Queue<int[]> queue = new LinkedList<>();
+        boolean visit[] = new boolean[n+1];
+        queue.add(new int[]{n, 0});
+        while(true){
+            int[] count = queue.poll();
+            if(count[0] == 1){
+                bw.write(count[1] + "\n");
+                break;
             }
-            if(i%2==0){
-                dp[i]=Math.min(dp[i/2]+1,dp[i]);
+
+            if(count[0] % 3 ==0 && !visit[count[0]/3]){
+                queue.add(new int[]{count[0]/3, count[1]+1});
+                visit[count[0]/3] = true;
             }
+            if(count[0] % 2 == 0 && !visit[count[0]/2]){
+                queue.add(new int[]{count[0]/2, count[1]+1});
+                visit[count[0]/2] = true;
+
+            }
+            if(!visit[count[0]-1]){
+                queue.add(new int[]{count[0] -1, count[1]+1});
+                visit[count[0]-1] = true;
+            }
+
         }
-        bw.write(String.valueOf(dp[num]));
         bw.flush();
-        bw.close();
-
-
-
-
     }
 
-
 }
-
-
-
