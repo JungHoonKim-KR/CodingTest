a92af48646a7c3c839fe82711fcb9650cff48de2
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-07 16:16:27 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -31,8 +31,7 @@ public class Main {
           dp[2] = arr[1] + arr[2];
 
         if (n >= 3) {
-            dp[3] = arr[3] + Math.max(arr[1],arr[2]);
-            for (int i = 4; i <= n; i++) {
+            for (int i = 3; i <= n; i++) {
                 dp[i] = arr[i] + Math.max(dp[i - 2], dp[i - 3] + arr[i - 1]);
             }
         }
