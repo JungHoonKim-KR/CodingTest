cd8f1e3037e316fff197f8f695cb659bf2d14c82
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-10 12:23:06 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 624 ms, Memory: 62300 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,49 +1,36 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static class Bit {
-        long[] tree;
-
-        Bit(int size) {
-            tree = new long[size + 1];
-        }
-
-        void update(int index, int value) {
-            while (index < tree.length) {
-                tree[index] += value;
-                index += index & -index;
-            }
-        }
-
-        long sum(int index) {
-            long sum = 0;
-            while (index > 0) {
-                sum += tree[index];
-                index -= index & -index;
-            }
-            return sum;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int testcase = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(bufferedReader.readLine());
-        Bit bit = new Bit(n);
-        for (int tc = 0; tc < n; tc++) {
-            bit.update(tc + 1, Integer.parseInt(st.nextToken()));
+        int m = Integer.parseInt(st.nextToken());
+        int arr[] = new int [n];
+        st = new StringTokenizer(br.readLine());
+        for(int i =0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        for(int tc = 0; tc<testcase;tc++){
-            st = new StringTokenizer(bufferedReader.readLine());
-            int firstIndex = Integer.parseInt(st.nextToken());
-            int lastIndex = Integer.parseInt(st.nextToken());
-            bufferedWriter.write(bit.sum(lastIndex) - bit.sum(firstIndex-1)+"\n");
+        int sum[] = new int [n];
+        sum[0] = arr[0];
+        for(int i =1; i<n; i++){
+            sum[i] = sum[i-1] + arr[i];
         }
-        bufferedWriter.close();
-        bufferedReader.close();
+
+
+        for(int i =0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+
+            bw.write(sectorSum(sum, arr, Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken())-1)+ "\n");
+        }
+
+        bw.flush();
     }
+
+    static int sectorSum(int sum[],int arr[],  int i, int j){
+        return sum[j] - sum[i] + arr[i];
+    }
+
 }
