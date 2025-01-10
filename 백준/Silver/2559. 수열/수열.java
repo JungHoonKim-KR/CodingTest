06289349371875025ed6079c21a1480e666e8985
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-10 13:18:49 +0900

    [Silver III] Title: 수열, Time: 252 ms, Memory: 24248 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -2,28 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Bit{
-        int arr[];
-        Bit(int size){
-            arr = new int[size+1];
-        }
 
-        void update(int index, int value){
-            while(index < arr.length){
-                arr[index] += value;
-                index += index & -index;
-            }
-        }
-
-        int sum(int index){
-            int sum=0;
-            while(index >0){
-                sum+=arr[index];
-                index -= index& -index;
-            }
-            return sum;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,14 +11,18 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        Bit bit = new Bit(n);
+        int sum [] = new int[n+1];
+        int arr[] = new int[n+1];
         st = new StringTokenizer(br.readLine());
-        for(int i =0; i<n; i++){
-            bit.update(i+1,Integer.parseInt(st.nextToken()));
+        for(int i =1; i<=n; i++){
+            arr[i] =  Integer.parseInt(st.nextToken());
+        }
+        for(int i =1; i<=n; i++){
+            sum[i] = sum[i-1] + arr[i];
         }
         int max = Integer.MIN_VALUE;
-        for(int i =1; i<=n-k+1; i++){
-            max = Math.max(max, bit.sum(i-1 +k) - bit.sum(i-1));
+        for(int i =1; i<=n -k +1; i++){
+            max = Math.max(max, sum[i-1+k] - sum[i-1]);
         }
         System.out.println(max);
     }
