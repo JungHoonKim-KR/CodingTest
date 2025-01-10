7cc0724e40e3091b9218e8d285c236455b146d54
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-10 12:39:31 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 580 ms, Memory: 53420 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -2,35 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static class Bit{
+        int tree[];
+        Bit(int size){
+            tree = new int[size+1];
+        }
+
+        void update(int index, int value){
+            while(index < tree.length){
+                tree[index] += value;
+                index += index & - index;
+            }
+        }
+
+        int sum(int index){
+            int sum=0;
+            while(index >0){
+                sum += tree[index];
+                index -= index & -index;
+            }
+
+            return sum;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int arr[] = new int [n];
+        Bit bit = new Bit(n);
         st = new StringTokenizer(br.readLine());
         for(int i =0; i<n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+            bit.update(i+1,  Integer.parseInt(st.nextToken()));
         }
-        int sum[] = new int [n];
-        sum[0] = arr[0];
-        for(int i =1; i<n; i++){
-            sum[i] = sum[i-1] + arr[i];
-        }
-
-
-        for(int i =0; i<m; i++){
+        for(int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
+            int first = Integer.parseInt(st.nextToken());
+            int second = Integer.parseInt(st.nextToken());
 
-            bw.write(sectorSum(sum, arr, Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken())-1)+ "\n");
+            bw.write(bit.sum(second) - bit.sum(first-1)+"\n");
         }
-
         bw.flush();
-    }
-
-    static int sectorSum(int sum[],int arr[],  int i, int j){
-        return sum[j] - sum[i] + arr[i];
     }
 
 }
