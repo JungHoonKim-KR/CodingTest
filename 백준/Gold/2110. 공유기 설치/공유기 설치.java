89a069b201215f2f5a0fda9d29b2c4fc49c2f8a9
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-01-14 15:21:59 +0900

    [Gold IV] Title: 공유기 설치, Time: 276 ms, Memory: 29664 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -12,10 +12,8 @@ public class Main {
 
         long points[] = new long [n];
 
-        long max = 0;
         for(int i =0; i<n; i++){
             long tree = Long.parseLong(br.readLine());
-            max = Math.max(max, tree);
             points[i] = tree;
         }
 
@@ -23,32 +21,23 @@ public class Main {
         Arrays.sort(points);
 
         // 집 좌표를 이용해서 각 집 사이의 거리를 구함.
-        long distances[] = new long [n-1];
 
-        for(int i = 1; i<n; i++){
-            distances[i-1] = points[i] - points[i-1];
-        }
 
-        long min = 0;
-        max ++;
+        long min = 1;
+        long max = points[n-1] - points[0] +1;
         while(min < max){
             long mid = (min + max)/2;
             // 카운팅이 중요함
             // 주어진 distances[]를 순서대로 조합해가며 mid가 넘지 않는 선에서 최대한 조합.
             // 조합된 거리를 count 1로 보고, count == C를 만족해야함.
-            long section=0;
+            long lastPoint= points[0];
             int count = 1;
-            for(long distance : distances){
-                if(section + distance <=mid){
-                    section += distance;
-                }
-                else {
-                    section = 0;
+            for(int i =1; i<n; i++){
+                if(points[i] - lastPoint >=mid){
+                    lastPoint = points[i];
                     count++;
                 }
-
             }
-
             // 해당 간격에서 설치될 수 있는 공유기가 적다면
             // 간격을 좁혀야함.
             if(count < c){
@@ -57,7 +46,7 @@ public class Main {
             else min = mid +1;
         }
 
-        System.out.println(min);
+        System.out.println(min-1);
     }
 
 
