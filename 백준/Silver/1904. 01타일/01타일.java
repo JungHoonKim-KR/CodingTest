7c9179b9917d7bd7b7c8c124b65e2801eb759b74
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-02-01 11:12:42 +0900

    [Silver III] Title: 01타일, Time: 124 ms, Memory: 18148 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -1,29 +1,29 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        long arr[] = new long[n + 1];
-        fib(arr, n);
-        bw.write(arr[n] + "");
-        bw.flush();
-    }
 
-    static void fib(long arr[], int n) {
-        arr[0] = 0;
-        arr[1] = 1;
+        int dp[] = new int[n+1];
+
+        dp[1] = 1;
+
         if(n>=2) {
-            arr[2] = 2;
+            dp[2] = 2;
+
             for (int i = 3; i <= n; i++) {
-                arr[i] = (arr[i - 1] + arr[i - 2]) % 15746;
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
             }
         }
+        System.out.print(dp[n]);
+
+
     }
+
 
 
 }
