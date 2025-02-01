987a0cfbb9a10bd8a77cdd560754921b6ea4a0c0
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-02-01 12:34:34 +0900

    [Silver II] Title: 연속합, Time: 224 ms, Memory: 23120 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,40 +1,39 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
-    static int max = Integer.MIN_VALUE;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int arr[] =new int[n];
+
+        int dp[] = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i =0;i<n;i++){
-            arr[i]=Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            dp[i] = Integer.parseInt(st.nextToken());
         }
-        find(arr);
+
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            if(sum + dp[i]>0){
+                sum += dp[i];
+                max = Math.max(max, sum);
+            }
+            else{
+                max = Math.max(max, dp[i]);
+                sum=0;
+            }
+        }
         bw.write(max+"");
+
         bw.flush();
-    }
+        br.close();
+        bw.close();
 
-    static void find(int arr[]){
-
-        int value=0;
-        for(int i =0; i<arr.length;i++){
-            if(value + arr[i]>0){
-                value+=arr[i];
-                max = Math.max(max,value);
-
-            }
-            else {
-                max = Math.max(max,arr[i]);
-                value=0;
-            }
-
-
-        }
     }
 
 
