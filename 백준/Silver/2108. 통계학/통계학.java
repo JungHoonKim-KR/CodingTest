fc2fb8cc487c18331aa03eeeeec48e2716767d48
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-03-23 13:47:45 +0900

    [Silver III] Title: 통계학, Time: 276 ms, Memory: 41076 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -1,123 +1,68 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.ArrayList;
-
-import java.util.List;
-import java.util.stream.IntStream;
+import java.util.*;
 
 
 public class Main {
-
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arr[] = new int[8001];
 
         int n = Integer.parseInt(br.readLine());
-        int arr[] = new int[n];
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        int midCount=0;
+        int midNumber=-4000;
+        int modeCount=0;
+        int modeMax=-4000;
+        for(int i = 0; i < n; i++){
+            int num = Integer.parseInt(br.readLine());
+            arr[num + 4000] ++;
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            sum += num;
+            min = Math.min(min, num);
+            max = Math.max(max, num);
 
         }
-        int max_arr=Arrays.stream(arr).max().getAsInt();
-        int count[] = new int[4002];
-        int minus_count[]=new int[4001];
-        Arrays.sort(arr);
 
-        //1번
-        System.out.println(Math.round(Arrays.stream(arr).average().getAsDouble()));
-        //2번
-        System.out.println(arr[n/2]);
-        //3번
-        int maxcount=0;
-        int counting_index=0;
-        int counting[]=new int[2];
-        counting[0]=4001;
-        counting[1]=4002;
+        boolean flag = false;
+        for(int i= min + 4000 ; i<= max + 4000; i++){
 
-        for (int j = 0; j < n; j++) {
-            if(arr[j]>=0) {
+            if(arr[i] >0){
+                // 중앙값
+                if(midCount < (n+1)/2){
+                    midCount += arr[i];
+                    midNumber = i - 4000;
+                }
 
-                count[arr[j]]++;
+                //최빈값
+                if(modeCount < arr[i]){
+                    modeCount = arr[i];
+                    modeMax = i-4000;
+                    flag = true;
+                }
+                else if(modeCount == arr[i] && flag){
+                    flag = false;
+                    modeMax = i - 4000;
+                }
 
             }
 
-            else if(arr[j]<0)
-                minus_count[Math.abs(arr[j])]++;
-        }
-
-        for(int z=0;z<count.length;z++){
-            maxcount=Math.max(maxcount,count[z]);
-            ;
 
 
         }
-        for(int Z=0;Z< minus_count.length;Z++){
-            maxcount=Math.max(maxcount,minus_count[Z]);
-
-        }
 
 
-        for(int k : arr){
-            if(k<0) {
-                if (minus_count[Math.abs(k)] == maxcount&&!IntStream.of(counting).anyMatch(x -> x == k)) {
-                    counting[counting_index] = k;
-
-                    counting_index++;
-
-
-                }
-                    if(counting_index==2) {
-                      System.out.println(counting[1]);
-
-                        break;
-                    }
-
-                }
-
-             else if(k>=0)  {
-                 if(count[k]==maxcount&&!IntStream.of(counting).anyMatch(x -> x == k)){
-                     counting[counting_index] =k ;
-
-                     counting_index++;
-                     if(counting_index==2) {
-                         System.out.println(counting[1]);
-                         break;
-                     }
-
-                 }
-            }
-
-        }
-
-        if(counting_index==1)
-            System.out.println(counting[0]);
-
-       //4번
-       int range=Arrays.stream(arr).max().getAsInt()-Arrays.stream(arr).min().getAsInt();
-        System.out.println(range);
-
-
-
-
-
+        System.out.println((int) Math.round((double)sum / n));
+        System.out.println(midNumber);
+        System.out.println(modeMax);
+        System.out.println(max - min);
 
     }
 
 
 
 
-
-
-
-
-    }
-
-
-
-
-
-
-
-
+}
