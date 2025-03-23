e47f0bafb69f88e7e7e5272f9acb93f5a2138c94
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-03-23 19:53:09 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 116 ms, Memory: 15668 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,69 +1,57 @@
 import java.io.*;
-import java.lang.String;
 import java.util.*;
 
 
-
 public class Main {
-    static int Max=Integer.MIN_VALUE;
-    static int Min=Integer.MAX_VALUE;
-    static int arr[];
-    static int oper[];
-    static int N;
-
+    static int numbers [];
+    static int ops[];
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //문제 받기
-        N=Integer.parseInt(br.readLine());
-        arr=new int [N];
-        oper=new int[4];
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        for(int i=0;i<N;i++){
-            arr[i]=Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        ops = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
+        dfs(1,numbers[0]);
+
+        System.out.println(max);
+        System.out.println(min);
+
+    }
+
+    static void dfs(int depth, int value){
+        if(depth == numbers.length){
+            max = Math.max(max, value);
+            min = Math.min(min, value);
+            return;
         }
-        st=new StringTokenizer(br.readLine());
-        for(int j=0;j<4;j++){
-            oper[j]=Integer.parseInt(st.nextToken());
-        }
-        dfs(arr[0],1);
-        System.out.println(Max);
-        System.out.println(Min);
 
-
-        }
-
-            public static void dfs(int value,int index){
-            if(index==N){
-                Max=Math.max(value,Max);
-                Min=Math.min(value,Min);
-                return;
-            }
-            for(int i=0;i<4;i++) {
-                if (oper[i] > 0) {
-                    oper[i]--;
-
-                    switch (i) {
-                        case 0:
-                            dfs(value+arr[index],index+1);
-                            break;
-                        case 1:
-                            dfs(value-arr[index],index+1);
-                            break;
-                        case 2:
-                            dfs(value*arr[index],index+1);
-                            break;
-                        case 3:
-                            dfs(value/arr[index],index+1);
-                            break;
-                    }
-                   oper[i]++;
+        for(int i =0; i<4; i++){
+            if(ops[i]>0){
+                ops[i] --;
+                switch (i){
+                    case 0:
+                        dfs(depth+1, value + numbers[depth]);
+                        break;
+                    case 1:
+                        dfs(depth+1, value - numbers[depth]);
+                        break;
+                    case 2:
+                        dfs(depth+1, value * numbers[depth]);
+                        break;
+                    case 3:
+                        dfs(depth+1, value / numbers[depth]);
+                        break;
                 }
-
-
+                ops[i] ++;
             }
+
         }
+
+
 
     }
 
@@ -72,31 +60,4 @@ public class Main {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
