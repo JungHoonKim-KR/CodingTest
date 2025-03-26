57f72cbe776c05f0f895679d9d2eb94203a0715b
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-03-26 14:00:48 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 820 ms, Memory: 121040 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -20,8 +20,6 @@ public class Main {
                 
             }
         }
-        int sum[][] = new int[n+1][n+1];
-
 
         for(int i=1; i<=n; i++){
             for(int j=1; j<=n; j++){
