3cddc4550cc663df677eb1f25cc2bfab05208d57
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-03-30 13:09:56 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 328 ms, Memory: 23940 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -1,37 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main {
 
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        int dp[][][]= new int[n+1][n+1][3];
-        for (int i = 1; i <= n; i++) {
+        int dp[][][] = new int[n][n][3];
+
+        for(int i =0; i<n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             dp[i][i][0] = Integer.parseInt(st.nextToken());
             dp[i][i][1] = Integer.parseInt(st.nextToken());
-
         }
-        for(int gap=1; gap<=n-1; gap++){
-            for(int start=1; start <=n-gap; start++){
-                int end = start + gap;
+
+//        dp[start][end][2] = dp[start][mid][2] + dp[mid+1][end][2] + (dp[start][start][0] * dp[mid][mid][0]*dp[end][end][1])
+        for(int end=1; end<n; end++){
+            for(int start=end-1; start>=0; start--){
                 dp[start][end][2] = Integer.MAX_VALUE;
                 for(int mid = start; mid<end; mid++){
-                    dp[start][end][2] = Math.min(dp[start][end][2],
-                            dp[start][mid][0] * dp[start][mid][1] * dp[mid+1][end][1]
-                            + dp[start][mid][2] + dp[mid+1][end][2]);
+                    int value = dp[start][mid][2] + dp[mid+1][end][2] + (dp[start][start][0] * dp[mid][mid][1]*dp[end][end][1]);
+                    if(dp[start][end][2] > value){
+                        dp[start][end][2] = value;
+                 
+                    }
                 }
-                dp[start][end][0] = dp[start][start][0];
-                dp[start][end][1] = dp[end][end][1];
             }
         }
-
-        System.out.println(dp[1][n][2]);
+        System.out.println(dp[0][n-1][2]);
     }
-
-
 }
