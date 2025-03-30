e8169d52ff6b9d0af7f440ceb860bab002a4ddec
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-03-30 13:10:53 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 304 ms, Memory: 26252 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -21,11 +21,7 @@ public class Main {
             for(int start=end-1; start>=0; start--){
                 dp[start][end][2] = Integer.MAX_VALUE;
                 for(int mid = start; mid<end; mid++){
-                    int value = dp[start][mid][2] + dp[mid+1][end][2] + (dp[start][start][0] * dp[mid][mid][1]*dp[end][end][1]);
-                    if(dp[start][end][2] > value){
-                        dp[start][end][2] = value;
-                 
-                    }
+                   dp[start][end][2] = Math.min( dp[start][end][2], dp[start][mid][2] + dp[mid+1][end][2] + (dp[start][start][0] * dp[mid][mid][1]*dp[end][end][1]));
                 }
             }
         }
