e264a52508e9ee802b7f1a930298036d2bdd6ee2
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-04-01 13:00:40 +0900

    [Gold III] Title: 오등큰수, Time: 908 ms, Memory: 138552 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.java
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -8,17 +8,17 @@ public class Main{
         int n = Integer.parseInt(br.readLine());
 
         int arr[] = new int [n];
-        Map<Integer, Integer> countMap = new HashMap<>();
+        int countArr[] =new int[1000001];
         Stack<Integer> stack = new Stack<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i =0; i<n; i++){
             int num = Integer.parseInt(st.nextToken());
             arr[i] = num;
-            countMap.put(num, countMap.getOrDefault(num, 0) +1);
+            countArr[num]++;
         }
 
         for(int i =0; i<n; i++){
-            while(!stack.isEmpty() && countMap.get(arr[stack.peek()]) < countMap.get(arr[i])){
+            while(!stack.isEmpty() && countArr[arr[stack.peek()]] < countArr[arr[i]]){
                 arr[stack.pop()] = arr[i];
             }
             stack.push(i);
