375c03010a1a3aaf53cafa4a6da7be3df6461a0b
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-04-04 13:15:46 +0900

    [Gold V] Title: 토마토, Time: 596 ms, Memory: 126604 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,79 +1,60 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static class Point {
-        int x, y, z;
-
-        Point(int x, int y, int z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-    }
+    static int goX[] = {-1, 1, 0, 0, 0, 0};
+    static int goY[] = {0, 0, -1, 1, 0, 0};
+    static int goH[] = {0, 0, 0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
 
-        int gox[] = {-1, 0, 1, 0, 0, 0};
-        int goY[] = {0, 1, 0, -1, 0, 0};
-        int goZ[] = {0, 0, 0, 0, 1, -1};
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int x = Integer.parseInt(st.nextToken());
+
         int y = Integer.parseInt(st.nextToken());
-        int z = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
 
-        int unripeCount = 0, max = 0;
-        int map[][][] = new int[z][y][x];
-        boolean visit[][][] = new boolean[z][y][x];
-        Queue<Point> queue = new LinkedList<>();
-        for (int k = 0; k < z; k++) {
-            for (int i = 0; i < y; i++) {
+        int arr[][][] = new int[x][y][h];
+        int unripedCount = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        for (int k = 0; k < h; k++) {
+            for (int i = 0; i < x; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < x; j++) {
-                    int stance = Integer.parseInt(st.nextToken());
-                    if (stance == 1) {
-                        queue.add(new Point(j, i, k));
-                        visit[k][i][j] = true;
-                    } else if (stance == 0) {
-                        unripeCount++;
+                for (int j = 0; j < y; j++) {
+                    int num = Integer.parseInt(st.nextToken());
+                    arr[i][j][k] = num;
+                    if (num == 0)
+                        unripedCount ++;
+                    else if (num == 1) {
+                        queue.add(new int[]{i, j, k});
                     }
-                    map[k][i][j] = stance;
                 }
             }
         }
-        //1: 출발지, -1: 막힌 길: 0: 아직 안 간 길
-
-        while (unripeCount > 0 && !queue.isEmpty()) {
-            Point poll = queue.poll();
-
+        int max = 1;
+        while (unripedCount > 0 && !queue.isEmpty()) {
+            int cur[] = queue.poll();
             for (int i = 0; i < 6; i++) {
-                int moveX = poll.x + gox[i];
-                int moveY = poll.y + goY[i];
-                int moveZ = poll.z + goZ[i];
-                if (moveX < 0 || moveX >= x || moveY < 0 || moveY >= y || moveZ < 0 || moveZ >= z || map[moveZ][moveY][moveX] == -1 || visit[moveZ][moveY][moveX])
+                int moveX = goX[i] + cur[0];
+                int moveY = goY[i] + cur[1];
+                int moveH = goH[i] + cur[2];
+                if (moveX < 0 || moveX >= x || moveY < 0 || moveY >= y || moveH < 0 || moveH >= h || arr[moveX][moveY][moveH] == -1 || arr[moveX][moveY][moveH] >= 1) {
                     continue;
-
-                if (--unripeCount == 0) {
-                    max = map[poll.z][poll.y][poll.x];
-                    break;
                 }
-                queue.add(new Point(moveX, moveY, moveZ));
-                visit[moveZ][moveY][moveX] = true;
-                map[moveZ][moveY][moveX] = map[poll.z][poll.y][poll.x] + 1;
+                unripedCount--;
+                arr[moveX][moveY][moveH] = arr[cur[0]][cur[1]][cur[2]] + 1;
+                max = Math.max(arr[moveX][moveY][moveH], max);
+                queue.add(new int[]{moveX, moveY, moveH});
             }
 
         }
-
-        if (unripeCount != 0) {
+        if (unripedCount != 0) {
             System.out.println(-1);
-        } else {
-            System.out.println(max);
-        }
-
+        } else
+            System.out.println(max - 1);
 
     }
+
+
 }
