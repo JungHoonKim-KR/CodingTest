49110b964829be813c2fc0caabfd53ea1cea6538
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-04-05 22:21:32 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 732 ms, Memory: 212480 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,83 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int goX[] = new int[]{-1, 0, 1, 0};
+        int goY[] = new int[]{0, -1, 0, 1};
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        char map [][] = new char [n][m];
-        int goX[] ={1,0,-1,0};
-        int goY[] ={0,1,0,-1};
 
-        boolean visit[][][] = new boolean[2][n][m];
-        //꺳으면 1
-        int arr[][] = new int[n][m];
-        arr[0][0] = 1;
+        boolean visit[][][] = new boolean[n][m][2];
+        char arr[][] = new char[n][m];
 
-       for(int i =0; i<n; i++){
-           String s = br.readLine();
-           for(int j =0; j<m; j++){
-               map[i][j] = s.charAt(j);
-           }
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine().toCharArray();
         }
-       Queue<int[]>queue = new LinkedList<>();
-       visit[0][0][0] = true;
-       queue.add(new int[]{0,0,0});
 
-       while(!queue.isEmpty()){
-           int[] poll = queue.poll();
-           for(int i =0; i<4; i++){
-               int moveX = poll[0] + goX[i];
-               int moveY = poll[1] + goY[i];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0, 1, 0});
 
-               if(moveX<0 || moveX>n-1 || moveY<0 || moveY>m-1)
-                   continue;
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
 
-               // 다음 칸이 벽일 떄
-               if(map[moveX][moveY]=='1'){
-                   // 벽을 이때까지 안부숨
-                   if(poll[2]==0 && !visit[0][moveX][moveY]){
-                       visit[0][moveX][moveY] = true;
-                       arr[moveX][moveY] = arr[poll[0]][poll[1]] +1;
-                       //이제 부숨
-                       queue.add(new int[]{moveX,moveY,1});
-                   }
-                   //이미 부순 경우는 어차피 없으므로 무시
-               }
-               else{
-                   if(poll[2]==0 && !visit[0][moveX][moveY]){
-                       visit[0][moveX][moveY] = true;
-                       arr[moveX][moveY] = arr[poll[0]][poll[1]] +1;
-                       queue.add(new int[]{moveX, moveY, 0});
-                   }
-                   else if(poll[2]==1 && !visit[1][moveX][moveY]){
-                       visit[1][moveX][moveY] = true;
-                       arr[moveX][moveY] = arr[poll[0]][poll[1]] +1;
-                       queue.add(new int[]{moveX, moveY, 1});
-                   }
-               }
+            if (!visit[cur[0]][cur[1]][cur[3]]) {
+                if (cur[0] == n - 1 && cur[1] == m - 1) {
+                    System.out.println(cur[2]);
+                    return;
+                }
+                visit[cur[0]][cur[1]][cur[3]] = true;
+                if (arr[cur[0]][cur[1]] == '1') {
+                    cur[3] = 1;
+                }
 
-               if(moveX == n-1 && moveY == m-1){
-                   System.out.println(arr[moveX][moveY]);
-                   return;
-               }
-           }
-       }
-       if(visit[1][n-1][m-1] || visit[0][n-1][m-1]){
-           System.out.println(arr[n-1][m-1]);
-       }
-       else
-            System.out.println(-1);
+                for (int i = 0; i < 4; i++) {
+                    int moveX = goX[i] + cur[0];
+                    int moveY = goY[i] + cur[1];
 
+                    if (moveX < 0 || moveX >= n || moveY < 0 || moveY >= m || visit[moveX][moveY][cur[3]] || (cur[3] == 1 && arr[moveX][moveY] == '1')) {
+                        continue;
+                    }
 
+                    queue.add(new int[]{moveX, moveY, cur[2] + 1, cur[3]});
+                }
+            }
+        }
 
+        System.out.println(-1);
     }
 }
