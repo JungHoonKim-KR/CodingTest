b8b5139acd97b68e02a702287554a8cf4f97a59d
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-04-15 12:12:04 +0900

    [Gold II] Title: 중앙값 구하기, Time: 224 ms, Memory: 18208 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
@@ -1,50 +1,45 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
         int tc = Integer.parseInt(br.readLine());
-
-        while (tc-- > 0) {
+        StringBuilder sb = new StringBuilder();
+        while(tc-->0){
             int n = Integer.parseInt(br.readLine());
-            sb.append((n + 1) / 2).append("\n"); // 중앙값 개수 출력
-
-            PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
-            int count = 0; // 전체 수열에서 몇 번째 수인지
-            int outputCount = 0; // 한 줄에 10개씩 출력 제어용
-
-            int totalLines = (n + 9) / 10; // ceil(n / 10)
-            for (int i = 0; i < totalLines; i++) {
-                int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                for (int num : input) {
+            PriorityQueue<Integer>leftQueue = new PriorityQueue<>((o1,o2)->{
+                return o2-o1;
+            });
+            PriorityQueue<Integer>rightQueue = new PriorityQueue<>();
+            sb.append((n+1)/2).append("\n");
+            int count=0;
+            for(int i =0; i<(n+9)/10;i++){
+                int input [] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+                for(int j=0; j<input.length; j++){
                     count++;
-                    q.add(num);
+                    leftQueue.add(input[j]);
 
-                    if (count % 2 == 1) { // 홀수 번째일 때만 중앙값 출력
-                        ArrayList<Integer> temp = new ArrayList<>();
-                        int midIdx = q.size() / 2;
-                        for (int k = 0; k < midIdx; k++) {
-                            temp.add(q.poll());
-                        }
-
-                        int median = q.peek(); // 중앙값 확인
-                        sb.append(median).append(" ");
-                        outputCount++;
-
-                        if (outputCount % 10 == 0) sb.append("\n");
-
-                        // 복원
-                        q.addAll(temp);
+                    if(!rightQueue.isEmpty() && leftQueue.peek() > rightQueue.peek()){
+                        int left = leftQueue.poll();
+                        int right = rightQueue.poll();
+                        rightQueue.add(left);
+                        leftQueue.add(right);
                     }
 
-                    if (count == n) break;
+                    if(leftQueue.size() - rightQueue.size() ==2)
+                        rightQueue.add(leftQueue.poll());
+                    if(count%2==1)
+                        sb.append(leftQueue.peek()+" ");
+
+                    if(count/2==10){
+                        sb.append("\n");
+                        count=0;
+                    }
                 }
             }
-            if (outputCount % 10 != 0) sb.append("\n");
+            sb.append("\n");
         }
+        System.out.println(sb);
 
-        System.out.print(sb);
     }
 }
