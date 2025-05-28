7d2624abfe029036ca96f700d74bf85c4d8a2c82
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-05-28 23:04:34 +0900

    [level 2] Title: 올바른 괄호, Time: 5.13 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,33 +2,11 @@ import java.io.*;
 import java.util.*;
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        if(s.charAt(0) == ')' || s.charAt(s.length()-1) =='('){
-            answer = false;
-            return answer;
-        }
-        
-        Stack<Character> stack = new Stack();
-        
+       int count = 0;
         for(int i =0; i<s.length(); i++){
-            stack.add(s.charAt(i));
+            count += (s.charAt(i) == '(')? 1 : -1;
+            if(count <0) return false;
         }
-        
-        int left=0, right=0;
-        
-        
-        while(!stack.isEmpty()){
-            char pop = stack.pop();
-            if(pop == '(')
-                left++;
-            else right++;
-            
-            if(right < left)
-                break;
-            
-        }
-        if(left != right)
-            answer = false;
-        return answer;
+        return count==0;
     }
 }
