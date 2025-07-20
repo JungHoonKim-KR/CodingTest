c21a433868f0cb32145df5edccd79af5d47397d8
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-07-20 13:04:45 +0900

    [Silver I] Title: 스타트와 링크, Time: 260 ms, Memory: 16596 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,63 +1,51 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.StringTokenizer;
 
 public class Main {
     static int arr[][];
     static int n;
-    static boolean visit[];
-    static int min = Integer.MAX_VALUE;
-
+    static int result=Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n + 1][n + 1];
-        visit = new boolean[n + 1];
-        for (int j = 1; j <= n; j++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 1; i <= n; i++) {
-                arr[j][i] = Integer.parseInt(st.nextToken());
-            }
+         n = Integer.parseInt(br.readLine());
+        arr = new int [n][n];
+        for(int i = 0; i<n; i++){
+            arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
-        dfs(0,1);
-        bw.write(min+"");
+        boolean[] booleans = new boolean[arr.length];
+        booleans[0] = true;
+        dfs(1, booleans, 1);
+
+        bw.write(result+"");
         bw.flush();
     }
-
-    static void dfs(int depth, int value) {
-        int team1=0, team2=0;
-        if(depth >= n/2){
-            for(int j=1; j<n; j++){
-                for(int i=j+1; i<=n; i++){
-                    if(visit[i] && visit[j]){
-                        team1 += arr[i][j] + arr[j][i];
+    static void dfs(int depth, boolean visited[], int index){
+        if(depth == n/2){
+            int sum1 =0,sum2=0;
+            for(int i=0; i<n-1; i++){
+                for(int j = i+1; j<n; j++){
+                    //team1
+                    if(visited[i] && visited[j]){
+                        sum1 += arr[i][j] + arr[j][i];
                     }
-                    else if(!visit[i] && !visit[j]){
-                        team2 += arr[i][j] + arr[j][i];
+                    else if(!visited[i] && !visited[j]){
+                        sum2 += arr[i][j] + arr[j][i];
                     }
                 }
             }
-
-            min = Math.min(min, Math.abs(team1 - team2));
+            result = Math.min(result, Math.abs(sum1 - sum2));
             return;
         }
 
-        for(int i = value; i<=n; i++){
-            if(!visit[i]) {
-                visit[i] = true;
-                dfs(depth + 1, i + 1);
-                visit[i] = false;
+        for(int i=index; i<n; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                dfs(depth+1, visited, i+1);
+                visited[i] = false;
             }
         }
-
-
-
     }
-
-
-
-
 
 }
