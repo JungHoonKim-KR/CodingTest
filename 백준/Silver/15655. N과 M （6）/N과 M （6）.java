16333980349ce32d54285706daba6a62cea70b7f
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-08-07 23:06:39 +0900

    [Silver III] Title: N과 M (6), Time: 132 ms, Memory: 16228 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int n, k;
+    static int arr[];
+    static boolean visit[];
+    static int result[];
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+        visit = new boolean[n];
+        result = new int[k];
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth, int start) {
+        if (depth == k) {
+            for (int i = 0; i < k; i++) {
+                sb.append(result[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                result[depth] = arr[i];
+                dfs(depth + 1,i+1);
+                visit[i] = false;
+            }
+        }
+    }
+}
+
+
