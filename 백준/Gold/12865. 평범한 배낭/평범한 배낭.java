9f08452fbc6b964f3a411de21c3289432fb619ba
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-08-09 20:51:28 +0900

    [Gold V] Title: 평범한 배낭, Time: 164 ms, Memory: 53808 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,44 +1,41 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
-    static class Item{
-        int g, v;
-        Item(int g, int v){
-            this.g = g;
-            this.v = v;
-        }
-    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int g = Integer.parseInt(st.nextToken());
-        Item list[] =new Item[n+1];
-        list[0] = new Item(0,0);
-        for(int i =1; i<=n; i++){
+        int w = Integer.parseInt(st.nextToken());
+
+        int arr[][] = new int[n+1][2];
+        int dp[][] = new int[n+1][w+1];
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            list[i] = new Item(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            arr[i] = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
         }
-        Arrays.sort(list, (i1, i2)->{
-            return Integer.compare(i1.g, i2.g);
-        });
-        int dp[][] = new int[n+1][g+1];
-        for(int i = 1; i<=n; i++){
-            for(int j=1; j<=g; j++){
-                Item cur = list[i];
-                if(cur.g <= j){
-                    dp[i][j] = Math.max(cur.v,Math.max(dp[i-1][j], dp[i-1][j-cur.g]+cur.v));
+
+        for(int i =1; i<=n; i++){
+            for(int j=1; j<=w; j++){
+                int cur[] = arr[i];
+                if(j>=cur[0]){
+                    dp[i][j] =Math.max(dp[i-1][j-cur[0]] + cur[1],dp[i-1][j]);
                 }
-                else dp[i][j] = dp[i-1][j];
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+
             }
         }
-        System.out.print(dp[n][g]);
+        System.out.println(dp[n][w]);
+
+
+
+
 
     }
-
-
 
 }
