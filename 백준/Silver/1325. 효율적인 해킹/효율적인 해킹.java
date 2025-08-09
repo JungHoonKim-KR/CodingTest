fd3f8a7f8707a6d07650d3ed1f836c32bf6ef262
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-08-09 17:09:20 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10060 ms, Memory: 145976 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -2,55 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static List<Integer> graph[];
+    static List<Integer>[] graph;
     static boolean visit[];
-    static int maxArr[];
-    static int n,m,c,p,max=0;
-
-    public static void main(String[] args) throws IOException {
+    static int[] maxArr;
+    static int max=0;
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        graph = new List[n + 1];
-        maxArr = new int[n + 1];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        graph =new ArrayList[n+1];
         for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
+        maxArr = new int[n+1];
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            c = Integer.parseInt(st.nextToken());
-            p = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
             graph[c].add(p);
         }
 
-        for (int i = 1; i <= n; i++) {
-            visit = new boolean[n + 1];
-            Queue<Integer> queue = new LinkedList<>();
-            queue.add(i);
-            visit[i] = true;
-            while (!queue.isEmpty()) {
-                int cur = queue.poll();
-                for (int next : graph[cur]) {
-                    if (!visit[next]) {
-                        visit[next] = true;
-                        queue.add(next);
-                        maxArr[next]++;
-                    }
-                }
-            }
+        for(int i = 1; i<=n; i++){
+            visit = new boolean[n+1];
+            dfs(i);
         }
-        for (int v : maxArr) {
-            max = Math.max(max, v);
+        for(int v : maxArr){
+            max = Math.max(max,v);
         }
-
-        for (int i = 1; i <= n; i++) {
-            if (max == maxArr[i]) {
+        for(int i = 1; i<=n; i++){
+            if(max == maxArr[i]){
                 sb.append(i).append(" ");
             }
         }
         System.out.println(sb);
+
+
+    }
+    static void dfs(int start){
+        visit[start] = true;
+        for(int next : graph[start]){
+            if(!visit[next]){
+                maxArr[next]++;
+                dfs(next);
+            }
+        }
     }
 
 
