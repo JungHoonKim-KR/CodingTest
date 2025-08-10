767bd075d8dc5945ce0a8586f7fb9569a9bb11cf
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-08-10 23:29:26 +0900

    [Silver I] Title: 효율적인 해킹, Time: 9848 ms, Memory: 146508 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -2,22 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static List<Integer>[] graph;
+    static List<Integer>graph[];
     static boolean visit[];
-    static int[] maxArr;
-    static int max=0;
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    static int cnt[];
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        graph =new ArrayList[n+1];
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+
+        graph = new ArrayList[n+1];
+        cnt = new int[n+1];
+        for(int i = 1; i<=n; i++){
+            graph[i] = new ArrayList();
         }
-        maxArr = new int[n+1];
-        for (int i = 0; i < m; i++) {
+
+        for(int i =0; i<m; i++){
             st = new StringTokenizer(br.readLine());
             int c = Integer.parseInt(st.nextToken());
             int p = Integer.parseInt(st.nextToken());
@@ -28,28 +29,27 @@ public class Main {
             visit = new boolean[n+1];
             dfs(i);
         }
-        for(int v : maxArr){
-            max = Math.max(max,v);
+        int max =0;
+        for(int i : cnt){
+            max = Math.max(max, i);
         }
+
         for(int i = 1; i<=n; i++){
-            if(max == maxArr[i]){
+            if(max == cnt[i])
                 sb.append(i).append(" ");
-            }
         }
         System.out.println(sb);
 
-
     }
+
     static void dfs(int start){
         visit[start] = true;
+        cnt[start]++;
         for(int next : graph[start]){
             if(!visit[next]){
-                maxArr[next]++;
                 dfs(next);
             }
         }
     }
-
-
 }
 
