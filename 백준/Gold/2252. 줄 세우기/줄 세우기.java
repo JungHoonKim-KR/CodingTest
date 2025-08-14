81141393db01ef11fa20ec2e0ef76614f5cffdf0
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-08-14 15:24:03 +0900

    [Gold III] Title: 줄 세우기, Time: 424 ms, Memory: 48920 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -2,51 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-
+	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        int indegree[] = new int[n + 1];
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList());
-        }
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-            graph.get(u).add(v);
-            indegree[v]++;
-        }
-
-        Queue<Integer>queue = new LinkedList<>();
-
-        // 시작 노드 추가
-        for(int i =1; i<=n; i++){
-            if(indegree[i]==0){
-                queue.add(i);
-            }
-        }
-
-        while(!queue.isEmpty()){
-            Integer poll = queue.poll();
-            bw.write(poll+" ");
-
-            for(Integer node : graph.get(poll)){
-                indegree[node] --;
-                if(indegree[node] ==0)
-                    queue.add(node);
-            }
-        }
-
-        bw.flush();
+        StringBuilder sb = new StringBuilder();
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	int n = Integer.parseInt(st.nextToken());
+    	int m = Integer.parseInt(st.nextToken());
+    	
+    	List<Integer>list[] = new ArrayList[n+1];
+    	int indegree[] = new int[n+1];
+    	for(int i = 1; i<=n; i++) {
+    		list[i] = new ArrayList();
+    	}
+    	
+    	while(m-->0) {
+    		st = new StringTokenizer(br.readLine());
+    		int a = Integer.parseInt(st.nextToken());
+    		int b = Integer.parseInt(st.nextToken());
+    		indegree[b]++;
+    		list[a].add(b);
+    	}
+    	
+    	Queue<Integer>q = new ArrayDeque();
+    	for(int i =1; i<=n; i++) {
+    		if(indegree[i] == 0)
+    			q.add(i);
+    	}
+    	
+    	while(!q.isEmpty()) {
+    		int cur = q.poll();
+    		sb.append(cur+" ");
+    		for(int next : list[cur]) {
+    			if(--indegree[next]==0)
+    				q.add(next);
+    		}
+    	}
+    	
+    	
+    	System.out.println(sb);
+    	
+    	
+    	
+    
     }
 }
