d47521b0b118728a1ba04423e45419a52a7c26e7
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-08-31 13:03:00 +0900

    [Gold V] Title: 토마토, Time: 572 ms, Memory: 104656 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,59 +1,54 @@
-import java.util.*;
+
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int goX[] = {-1, 1, 0, 0, 0, 0};
-    static int goY[] = {0, 0, -1, 1, 0, 0};
-    static int goH[] = {0, 0, 0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int y = Integer.parseInt(st.nextToken());
-        int x = Integer.parseInt(st.nextToken());
-        int h = Integer.parseInt(st.nextToken());
-
-        int arr[][][] = new int[x][y][h];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[][][] map = new int[K][M][N];
+        Queue<int[]> q = new ArrayDeque<>();
         int unripedCount = 0;
-        Queue<int[]> queue = new LinkedList<>();
-        for (int k = 0; k < h; k++) {
-            for (int i = 0; i < x; i++) {
+        for (int i = 0; i < K; i++) {
+            for (int j = 0; j < M; j++) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < y; j++) {
-                    int num = Integer.parseInt(st.nextToken());
-                    arr[i][j][k] = num;
-                    if (num == 0)
-                        unripedCount ++;
-                    else if (num == 1) {
-                        queue.add(new int[]{i, j, k});
+                for (int k = 0; k < N; k++) {
+                    int input = Integer.parseInt(st.nextToken());
+                    map[i][j][k] = input;
+                    if (input == 1) {
+                        q.add(new int[]{i, j,k, 0});
                     }
+                    else if (input == 0)
+                        unripedCount++;
                 }
             }
         }
-        int max = 1;
-        while (unripedCount > 0 && !queue.isEmpty()) {
-            int cur[] = queue.poll();
+        int day = 0;
+        int[][] go = {{-1, 0,0}, {1, 0,0}, {0, -1,0}, {0, 1,0},
+                         {0, 0,-1}, {0,0,1}} ;
+        while (unripedCount != 0 && !q.isEmpty()) {
+            int[] cur = q.poll();
             for (int i = 0; i < 6; i++) {
-                int moveX = goX[i] + cur[0];
-                int moveY = goY[i] + cur[1];
-                int moveH = goH[i] + cur[2];
-                if (moveX < 0 || moveX >= x || moveY < 0 || moveY >= y || moveH < 0 || moveH >= h || arr[moveX][moveY][moveH] == -1 || arr[moveX][moveY][moveH] >= 1) {
-                    continue;
-                }
+                int moveZ = cur[0] + go[i][0];
+                int moveX = cur[1] + go[i][1];
+                int moveY = cur[2] + go[i][2];
+                if (moveX < 0 || moveX >= M || moveY < 0 || moveY >= N || moveZ <0 || moveZ >= K
+                        || map[moveZ][moveX][moveY] != 0) continue;
                 unripedCount--;
-                arr[moveX][moveY][moveH] = arr[cur[0]][cur[1]][cur[2]] + 1;
-                max = Math.max(arr[moveX][moveY][moveH], max);
-                queue.add(new int[]{moveX, moveY, moveH});
+                day = Math.max(day, cur[3] + 1);
+                map[moveZ][moveX][moveY] = cur[3] + 1;
+                q.add(new int[]{moveZ, moveX,moveY, cur[3] + 1});
             }
-
         }
         if (unripedCount != 0) {
             System.out.println(-1);
         } else
-            System.out.println(max - 1);
-
+            System.out.println(day);
     }
 
 
