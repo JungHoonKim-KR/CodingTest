1d35885545a0f9232e82285ed5b50503d0eee173
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-09-04 17:01:39 +0900

    [Unrated] Title: [모의 SW 역량테스트] 원자 소멸 시뮬레이션, Time: 2,676 ms, Memory: 123,364 KB -BaekjoonHub

--- SWEA/Unrated/5648. ［모의 SW 역량테스트］ 원자 소멸 시뮬레이션/［모의 SW 역량테스트］ 원자 소멸 시뮬레이션.java
+++ SWEA/Unrated/5648. ［모의 SW 역량테스트］ 원자 소멸 시뮬레이션/［모의 SW 역량테스트］ 원자 소멸 시뮬레이션.java
@@ -18,7 +18,6 @@ public class Solution {
     static int[][] go = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
     static int N, result;
     static List<Hack> hackList;
-    static Hack[][] hackMap = new Hack[4001][4001];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,7 +32,6 @@ public class Solution {
                 hackList.add(new Hack((Integer.parseInt(st.nextToken()) + 1000) * 2, (Integer.parseInt(st.nextToken()) + 1000) * 2, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
             }
             run();
-//            System.out.println("#" + t + " " + result);
             sb.append("#").append(t).append(" ").append(result).append("\n");
         }
         System.out.println(sb);
@@ -43,35 +41,29 @@ public class Solution {
     static void run() {
         // 이동
         while (!hackList.isEmpty()) {
-            for (int i = hackList.size() - 1; i >= 0; i--) {
-                Hack hack = hackList.get(i);
-                // 이전 턴에서 핵분열이 안된 핵 없애기
-                hackMap[hack.y][hack.x] = null;
-                if(hack.power == 0){
-                    hackList.remove(i);
-                    continue;
-                }
-
-                // 이동
+            HashMap<Integer, ArrayList<Hack>> hackMap = new HashMap<>();
+            for (Hack hack : hackList) {
                 hack.y += go[hack.dir][0];
                 hack.x += go[hack.dir][1];
 
-                // 범위 벗어나면 제거
-                if (hack.y < 0 || hack.y > 4000 || hack.x < 0 || hack.x > 4000) {
-                    hackList.remove(i);
+                if (hack.x < 0 || hack.x >= 4001 || hack.y < 0 || hack.y >= 4001) {
                     continue;
                 }
+                int key = hack.x * 4001 + hack.y;
+                hackMap.computeIfAbsent(key, k -> new ArrayList<>()).add(hack);
 
-                // 비어있으면 넣기
-                if (hackMap[hack.y][hack.x] == null) {
-                    hackMap[hack.y][hack.x] = hack;
+            }
+            List<Hack> tempList = new LinkedList<>();
+            for (List<Hack> hacks : hackMap.values()) {
+                if (hacks.size() >= 2) {
+                    for (Hack hack : hacks) {
+                        result += hack.power;
+                    }
                 } else {
-                    Hack lastHack = hackMap[hack.y][hack.x];
-                    result += lastHack.power + hack.power;
-                    lastHack.power = 0;
-                    hackList.remove(hack);
+                    tempList.add(hacks.get(0));
                 }
             }
+            hackList = tempList;
         }
     }
 
