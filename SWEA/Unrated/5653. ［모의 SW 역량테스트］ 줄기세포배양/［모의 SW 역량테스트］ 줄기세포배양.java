7313116e21601ec10e2573e61ebb389dda9ab47e
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-09-09 23:03:58 +0900

    [Unrated] Title: [모의 SW 역량테스트] 줄기세포배양, Time: 283 ms, Memory: 67,604 KB -BaekjoonHub

--- SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
+++ SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
@@ -6,9 +6,11 @@ import java.io.*;
  즉 배열 크기는 350 X 350 (N<=50, M<=50)
  */
 
-class Node{
+// 메모리 : 66,868 kb, 실행시간 : 322ms
+class Node {
     int x, y, originTime, activeStartTime, activeTime;
     boolean isBreed;
+
     Node(int x, int y, int originTime, int activeStartTime, int activeTime) {
         this.x = x;
         this.y = y;
@@ -18,16 +20,19 @@ class Node{
         this.isBreed = false;
     }
 }
+
 public class Solution {
     static int N, M, K, TIME;
-    static int[][] go = {{-1,0},{1,0},{0,-1},{0,1}};
-
+    static int[][] go = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+    static PriorityQueue<Node> pq;
+    static PriorityQueue<Node> breedQueue;
+    static boolean[][] visit;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
 
-        for(int tc = 1; tc <= T; tc++) {
+        for (int tc = 1; tc <= T; tc++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
@@ -36,54 +41,47 @@ public class Solution {
             TIME = 1;
             // 입력 값들의 좌표에서 + 150씩 해야함
             // x,y,time
-            PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2)->{
-                if(n1.activeStartTime == n2.activeStartTime){
+            pq = new PriorityQueue<>((n1, n2) -> {
+                if (n1.activeStartTime == n2.activeStartTime) {
                     // 생명력이 높은 순서
-                    return  Integer.compare(n2.originTime,n1.originTime);
+                    return Integer.compare(n2.originTime, n1.originTime);
                 }
-                return  Integer.compare(n1.activeStartTime,n2.activeStartTime);
+                return Integer.compare(n1.activeStartTime, n2.activeStartTime);
             });
-            PriorityQueue<Node>breedQueue = new PriorityQueue<>((n1, n2)->n2.originTime - n1.originTime);
+            breedQueue = new PriorityQueue<>((n1, n2) -> n2.originTime - n1.originTime);
 
-            boolean[][] visit = new boolean[350][350];
-            for(int i = 0; i<N; i++){
+            visit = new boolean[350][350];
+            for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
-                for(int j = 0; j<M; j++){
+                for (int j = 0; j < M; j++) {
                     int time = Integer.parseInt(st.nextToken());
-                    if(time != 0){
-                        pq.add(new Node(i+150, j+150, time, time, time));
-                        visit[i+150][j+150] = true;
+                    if (time != 0) {
+                        pq.add(new Node(i + 150, j + 150, time, time, time));
+                        visit[i + 150][j + 150] = true;
                     }
 
                 }
             }
 
-            while(TIME<=K){
-                List<Node>tempList = new ArrayList<>();
-                while (!breedQueue.isEmpty()){
+
+
+            while (TIME <= K) {
+                List<Node> tempList = new ArrayList<>();
+                while (!breedQueue.isEmpty()) {
                     Node cur = breedQueue.poll();
                     // 아직 번식 안 한 애들 있을 때
-                    if(!cur.isBreed){
-                        cur.isBreed = true;
-                        for(int j = 0; j<4; j++){
-                            int moveX = cur.x + go[j][0];
-                            int moveY = cur.y + go[j][1];
-
-                            if(!visit[moveX][moveY]){
-                                pq.add(new Node(moveX, moveY, cur.originTime, cur.activeStartTime + cur.originTime +1, cur.originTime));
-                                visit[moveX][moveY] = true;
-                            }
-                        }
+                    if (!cur.isBreed) {
+                        breed(cur);
                     }
                     cur.activeTime--;
-                    if(cur.activeTime > 0){
+                    if (cur.activeTime > 0) {
                         tempList.add(cur);
                     }
                 }
                 breedQueue.addAll(tempList);
 
                 // 번식 가능
-                while(!pq.isEmpty() && pq.peek().activeStartTime == TIME){
+                while (!pq.isEmpty() && pq.peek().activeStartTime == TIME) {
                     breedQueue.add(pq.poll());
                 }
 
@@ -93,6 +91,19 @@ public class Solution {
             sb.append("#").append(tc).append(" ").append(pq.size() + breedQueue.size()).append("\n");
         }
         System.out.println(sb);
+    }
+    static void breed(Node cur){
+        cur.isBreed = true;
+        for (int j = 0; j < 4; j++) {
+            int moveX = cur.x + go[j][0];
+            int moveY = cur.y + go[j][1];
+
+            if (!visit[moveX][moveY]) {
+                pq.add(new Node(moveX, moveY, cur.originTime, cur.activeStartTime + cur.originTime + 1,
+                        cur.originTime));
+                visit[moveX][moveY] = true;
+            }
+        }
     }
 
 
