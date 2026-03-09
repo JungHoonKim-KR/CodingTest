25c98987035d7463c5d7d3afabc58129da1198ce
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2026-03-09 23:01:05 +0900

    [Gold V] Title: 상범 빌딩, Time: 168 ms, Memory: 17576 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
@@ -1,83 +1,98 @@
+
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 /*
-    4방 탐색
-    지점 값이 -1 or 1이면 진행 X
+해당 좌표가 큰 사각형 기준 몇 사분면에 속하는지 확인
+한 사분면의 길이 : 2^N, 즉
  */
 public class Main {
-    static int N, M, K;
-    static BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+    static int L, N, M, startL, startX, startY, endL, endX, endY;
     static char[][][] map;
-    static int[][] go = {{-1, 0, 0}, {0, 1, 0}, {1, 0, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1}};
+    static int[][][] myMap;
+    static int[][] go = {{-1, 0, 0}, {1, 0, 0}, {0, -1, 0}, {0, 1, 0}, {0, 0, -1}, {0, 0, 1}};
+    static Queue<int[]> fireQueue = new ArrayDeque<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st;
-        int startK = 0, startN = 0, startM = 0;
-
         while (true) {
-            st = new StringTokenizer(br.readLine());
-            K = Integer.parseInt(st.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            L = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
 
-            if (K == 0 && N == 0 && M == 0) break;
-
-            map = new char[K][N][M];
-            //입력
-            for (int k = 0; k < K; k++) {
-                for (int i = 0; i < N; i++) {
-                    char[] input = br.readLine().toCharArray();
-                    for (int j = 0; j < M; j++) {
-                        char data = input[j];
-                        map[k][i][j] = data;
-                        if (data == 'S') {
-                            startK = k;
-                            startN = i;
-                            startM = j;
+            if (L == 0 && N == 0 && M == 0) {
+                return;
+            }
+            map = new char[L][N][M];
+            myMap = new int[L][N][M];
+            for (int i = 0; i < L; i++) {
+                for (int j = 0; j < N; j++) {
+                    Arrays.fill(myMap[i][j], Integer.MAX_VALUE);
+                }
+            }
+            for (int i = 0; i < L; i++) {
+                for (int j = 0; j < N; j++) {
+                    char[] inputArr = br.readLine().toCharArray();
+                    for (int k = 0; k < M; k++) {
+                        char c = inputArr[k];
+                        map[i][j][k] = c;
+                        if (c == 'S') {
+                            startL = i;
+                            startX = j;
+                            startY = k;
                         }
+                        if (c == 'E') {
+                            endL = i;
+                            endX = j;
+                            endY = k;
+                        }
+
                     }
                 }
                 br.readLine();
-            }
 
-            bfs(startK, startN, startM);
+            }
+            bfs();
+
+            if (myMap[endL][endX][endY] == Integer.MAX_VALUE) {
+                System.out.println("Trapped!");
+            } else System.out.println("Escaped in " + myMap[endL][endX][endY] + " minute(s).");
+
         }
-        bw.flush();
+
 
     }
 
-    static void bfs(int startK, int startN, int startM) throws IOException {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{startK, startN, startM, 0});
+    static void bfs() {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{startL, startX, startY});
+        myMap[startL][startX][startY] = 0;
+
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
+
             for (int i = 0; i < 6; i++) {
-                int nk = cur[0] + go[i][0];
-                int nn = cur[1] + go[i][1];
-                int nm = cur[2] + go[i][2];
-                if (nk < 0 || nk >= K || nn < 0 || nn >= N || nm < 0 || nm >= M
-                        || map[nk][nn][nm] == '#' || map[nk][nn][nm] == 'S') continue;
+                int moveL = cur[0] + go[i][0];
+                int moveX = cur[1] + go[i][1];
+                int moveY = cur[2] + go[i][2];
 
-                if (map[nk][nn][nm] == 'E') {
-                    int result = cur[3] + 1;
-                    bw.write("Escaped in " + result + " minute(s).\n");
-                    return;
-                } else {
-                    queue.add(new int[]{nk, nn, nm, cur[3] + 1});
-                    map[nk][nn][nm] = '#';
+                if (moveL < 0 || moveL >= L || moveX < 0 || moveX >= N || moveY < 0 || moveY >= M
+                        || map[moveL][moveX][moveY] == '#')
+                    continue;
+
+                if (myMap[moveL][moveX][moveY] > myMap[cur[0]][cur[1]][cur[2]] + 1) {
+                    myMap[moveL][moveX][moveY] = myMap[cur[0]][cur[1]][cur[2]] + 1;
+                    queue.add(new int[]{moveL, moveX, moveY});
                 }
-
-
             }
         }
-        bw.write("Trapped!\n");
+
+
     }
 
-}
 
+}
