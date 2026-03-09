87720e2d5921aa449f4c8473f3ed62947d795fe3
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2026-03-10 00:03:26 +0900

    [Gold IV] Title: 플로이드, Time: 392 ms, Memory: 43280 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,62 +1,56 @@
 import java.io.*;
 import java.util.*;
 
+/*
+    4방 탐색
+    지점 값이 -1 or 1이면 진행 X
+ */
 public class Main {
-    static class Node {
-        int from, to, weight;
-
-        Node(int f, int t, int w) {
-            from = f;
-            to = t;
-            weight = w;
-        }
-    }
+    static int N;
+    static long arr[][];
 
     public static void main(String[] args) throws IOException {
-        long INF = Long.MAX_VALUE /2;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-
-        int v = Integer.parseInt(br.readLine());
-        int e = Integer.parseInt(br.readLine());
-
-        long dist[][] = new long[v+1][v+1];
-        for(int i =1; i<=v; i++){
-            Arrays.fill(dist[i],INF);
-            dist[i][i] =0;
+        N = Integer.parseInt(br.readLine());
+        arr = new long[N + 1][N + 1];
+        int M = Integer.parseInt(br.readLine());
+        for (int i = 1; i <=N; i++) {
+            Arrays.fill(arr[i], Integer.MAX_VALUE);
+            arr[i][i] = 0;
         }
-
-        for(int i = 0; i<e; i++){
+        for (int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            dist[from][to] = Math.min(dist[from][to], w);
+            int cost = Integer.parseInt(st.nextToken());
+            arr[from][to] = Math.min(arr[from][to], cost);
         }
 
-        for(int k = 1 ; k<=v; k++){
-            for(int i = 1; i<=v; i++){
-                for(int j =1; j<=v; j++){
-                    if(dist[i][k] != INF && dist[k][j] != INF){
-                        dist[i][j] = Math.min(dist[i][k] + dist[k][j],dist[i][j]);
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (arr[i][k] + arr[k][j] < arr[i][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
                     }
                 }
             }
         }
 
-        for(int i =1; i<=v; i++){
-            for(int j =1; j<=v; j++){
-                if(dist[i][j] == INF){
-                    sb.append(0+" ");
-                }
-                else sb.append(dist[i][j]+" ");
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (arr[i][j] == Integer.MAX_VALUE)
+                    bw.write(0 + " ");
+                else
+                    bw.write(arr[i][j] + " ");
             }
-            sb.append("\n");
+            bw.write("\n");
         }
 
-        System.out.print(sb);
+        bw.flush();
+
     }
 
 
 }
+
