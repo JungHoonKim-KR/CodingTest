b1a51f8aa58818b430898a4c60195412b63c402f
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2026-04-05 10:18:09 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,30 +1,30 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-/*
-북쪽을 시작으로 반시계 방향으로 탐색
-갈 곳이 없으면 후진, 만약 벽이면 후진 못하고 그대로 정지
- */
+
 public class Main {
-    static int[][] go = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    static int[][] map;
-    static int N, M, X, Y, dir,count;
+    static int N, M;
+    static int go[][] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    static int map[][];
+    static int result = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        // init
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        X = Integer.parseInt(st.nextToken());
-        Y = Integer.parseInt(st.nextToken());
-        dir = Integer.parseInt(st.nextToken());
-
         map = new int[N][M];
+        int initX, initY, initDir;
+        st = new StringTokenizer(br.readLine());
+        initX = Integer.parseInt(st.nextToken());
+        initY = Integer.parseInt(st.nextToken());
+        initDir = Integer.parseInt(st.nextToken());
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
@@ -32,64 +32,77 @@ public class Main {
             }
         }
 
-        run();
-        System.out.println(count);
+        solve(initX, initY, initDir);
+
+        System.out.println(result);
+
 
     }
 
-    static void run() {
-        // 현재 방향 + for i _(4)
-        // 1 : 벽, 0 : 청소 필요, -1 : 청소 끝(벽은 아님)
+    static void solve(int x, int y, int dir) {
+
+        int curX = x, curY = y, curDir = dir;
+        outer:
         while (true) {
-            if(map[X][Y] == 0){
-                map[X][Y] = -1;
-                count++;
+            // 현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소한다
+
+            if (map[curX][curY] == 0) {
+                map[curX][curY] = 2;
+                result++;
             }
 
-            boolean isMove = false;
+            // 현재 칸의 주변
+            boolean dirty = false;
             for (int i = 0; i < 4; i++) {
-                int turnDir = (dir + i) % 4;
-                int moveX = X + go[turnDir][0];
-                int moveY = Y + go[turnDir][1];
-
-                if (moveX < 0 || moveX >= N || moveY < 0 || moveY >= M
-                        || map[moveX][moveY] != 0) {
+                //$4$칸 중 청소되지 않은 빈 칸이 있는 경우,
+                //반시계 방향으로
+                //$90^\circ$ 회전한다.
+                int nX = curX + go[i][0];
+                int nY = curY + go[i][1];
+                if (nX < 0 || nX >= N || nY < 0 || nY >= M || map[nX][nY] == 1 || map[nX][nY] == 2)
                     continue;
+
+                if (map[nX][nY] == 0) {
+                    dirty = true;
+                    break;
                 }
-                isMove = true;
-                break;
+                //바라보는 방향을 기준으로 앞쪽 칸이 청소되지 않은 빈 칸인 경우 한 칸 전진한다.
             }
 
-            if (!isMove) {
-                int reverseDir = (dir + 2) % 4;
-                int moveX = X + go[reverseDir][0];
-                int moveY = Y + go[reverseDir][1];
+            if (dirty) {
+                for (int i = 0; i < 4; i++) {
+                    curDir = (curDir + 3) % 4;
+                    int nX = curX + go[curDir][0];
+                    int nY = curY + go[curDir][1];
+                    if (nX < 0 || nX >= N || nY < 0 || nY >= M || map[nX][nY] == 1 || map[nX][nY] == 2)
+                        continue;
 
-                if (moveX < 0 || moveX >= N || moveY < 0 || moveY >= M
-                        || map[moveX][moveY] == 1)
-                    return;
+                    if (map[nX][nY] == 0) {
+                        curX = nX;
+                        curY = nY;
+                        break;
+                    }
+                }
+            }
 
-                // 후진
-                X = moveX;
-                Y = moveY;
-                //방향은 유지
+            else {
+                //바라보는 방향을 유지한 채로 한 칸 후진할 수 있다면 한 칸 후진하고 1번으로 돌아간다.
+                int nX = curX - go[curDir][0];
+                int nY = curY - go[curDir][1];
+                //바라보는 방향의 뒤쪽 칸이 벽이라 후진할 수 없다면 작동을 멈춘다.
+                if (nX < 0 || nX >= N || nY < 0 || nY >= M || map[nX][nY] == 1) {
+                    break outer;
+                }
+
+                curX = nX;
+                curY = nY;
 
             }
-            else{
-                dir = (dir+3) %4;
 
-                int moveX = X + go[dir][0];
-                int moveY = Y + go[dir][1];
-
-                if(moveX <0 || moveX >= N || moveY <0 || moveY >= M
-                || map[moveX][moveY] !=0)
-                    continue;
-
-                X = moveX;
-                Y = moveY;
-            }
 
         }
+
+
     }
 
 
